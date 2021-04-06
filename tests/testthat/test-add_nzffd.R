d70a5be1a65e10aac662047d39aba765d1c20657
Finn <lee.finnbar@gmail.com>
2021-04-07 09:27:58 +1200

    revert testthat to CRAN version

---
+++ tests/testthat/test-add_nzffd.R
@@ -0,0 +1,8 @@
+testthat::skip_on_cran()
+test_that("nzffd_add works", {
+  ad1 <- nzffd_add(nzffd_data)
+  expect_match(paste(colnames(ad1), collapse = " "), "GEOLOGY")
+  expect_match(paste(colnames(ad1), collapse = " "), "SPRING")
+  expect_match(paste(colnames(ad1), collapse = " "), "ORDER")
+  expect_match(paste(colnames(ad1), collapse = " "), "VLY_LNDFRM")
+  })
