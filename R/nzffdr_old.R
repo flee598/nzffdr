1c7a1d083793fba52bda8f3b30ec6d6ebbc4f513
flee598 <lee.finnbar@gmail.com>
2022-01-17 15:55:47 +1300

    print error for old funcs

---
+++ R/nzffdr_old.R
@@ -0,0 +1,33 @@
+#' Fill missing data (depreciated)
+#'
+#' Fill in missing NZFFD data. This function is now depreciated. See the \href{https://flee598.github.io/nzffdr/}{help page} for details.
+#'
+#' @param ... arguments to pass to function.
+#'
+#' @return A dataframe where each row is a NZFFD record.
+#'
+#' @examples
+#' \dontrun{
+#' nzffd_fill()
+#' }
+#' @export
+nzffd_fill <- function(...) {
+    stop("Function is depreciated, see https://flee598.github.io/nzffdr for details.")
+}
+
+#' Add missing data (depreciated)
+#'
+#' Add in missing NZFFD data. This function is now depreciated. See the \href{https://flee598.github.io/nzffdr/}{help page} for details.
+#'
+#' @param ... arguments to pass to function.
+#'
+#' @return A dataframe where each row is a NZFFD record.
+#'
+#' @examples
+#' \dontrun{
+#' nzffd_add()
+#' }
+#' @export
+nzffd_add <- function(...) {
+  stop("Function is depreciated, see https://flee598.github.io/nzffdr for details.")
+}
